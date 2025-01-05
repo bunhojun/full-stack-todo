2995e8f0d769efdd6711559c816593b38e44d95a
bunhojun <ponnponn.p@gmail.com>
2025-01-05 18:34:55 +0900

    add example value on swagger

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,16 +1,29 @@
 import { IsEmail, IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { UserRole } from '@/users/types/user-role.type';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'test name',
+  })
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    example: 'test@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'password',
+  })
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    example: 'normal',
+  })
   @IsOptional()
   @IsIn(Object.values(UserRole))
   role?: UserRole;
